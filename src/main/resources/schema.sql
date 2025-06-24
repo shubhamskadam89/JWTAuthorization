39076549baeb03ca86a018896becc8bd4bda4cc2
shubhamk005 <shubhamskadam89@gmail.com>
2025-06-24 11:04:18 +0530

    JWT Implementation with PostgreSQL

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,26 @@
-create table users(username varchar_ignorecase(50) not null primary key,password varchar_ignorecase(500) not null,enabled boolean not null);
-create table authorities (username varchar_ignorecase(50) not null,authority varchar_ignorecase(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
-create unique index ix_auth_username on authorities (username,authority);
+--create table users(username varchar_ignorecase(50) not null primary key,
+--                    password varchar_ignorecase(500) not null,
+--                    enabled boolean not null);
+--
+--create table authorities (username varchar_ignorecase(50) not null,
+--                            authority varchar_ignorecase(50) not null,
+--                            constraint fk_authorities_users foreign key(username) references users(username));
+--
+--create unique index ix_auth_username on authorities (username,authority);
+
+DROP TABLE IF EXISTS authorities;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    username VARCHAR(50) NOT NULL PRIMARY KEY,
+    password VARCHAR(500) NOT NULL,
+    enabled BOOLEAN NOT NULL
+);
+
+CREATE TABLE authorities (
+    username VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES users (username)
+);
+
+CREATE UNIQUE INDEX ix_auth_username ON authorities (username, authority);
